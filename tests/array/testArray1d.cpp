14de62b89c435c315587009421d7af5fe32537d8
Markus Salasoo <salasoo1@llnl.gov>
2018-03-01 18:11:55 -0800

    test array1d pointers, operators

--- tests/array/testArray1d.cpp
+++ tests/array/testArray1d.cpp
@@ -196,6 +196,138 @@ TEST_F(Array1dTest, when_swapTwoEntries_then_valuesSwapped)
   EXPECT_EQ(VAL, arr(1));
 }
 
+TEST_F(Array1dTest, given_entryPointer_when_dereference_then_originalValueReturned)
+{
+  array1d arr(8, ZFP_RATE_PARAM_BITS);
+  arr(0) = VAL;
+
+  double d = *(&arr(0));
+
+  EXPECT_EQ(VAL, d);
+}
+
+TEST_F(Array1dTest, given_entryPointer_when_postIncrement_then_ptrAdvancedAfterEval)
+{
+  array1d arr(8, ZFP_RATE_PARAM_BITS);
+  arr(1) = VAL;
+
+  array1d::pointer p = &arr(0);
+  double d = *p++;
+
+  EXPECT_EQ(0, d);
+  EXPECT_EQ(VAL, *p);
+}
+
+TEST_F(Array1dTest, given_entryPointer_when_postDecrement_then_ptrAdvancedAfterEval)
+{
+  array1d arr(8, ZFP_RATE_PARAM_BITS);
+  arr(0) = VAL;
+
+  array1d::pointer p = &arr(1);
+  double d = *p--;
+
+  EXPECT_EQ(0, d);
+  EXPECT_EQ(VAL, *p);
+}
+
+TEST_F(Array1dTest, given_entryPointer_when_preIncrement_then_ptrAdvancedBeforeEval)
+{
+  array1d arr(8, ZFP_RATE_PARAM_BITS);
+  arr(1) = VAL;
+
+  array1d::pointer p = &arr(0);
+
+  EXPECT_EQ(VAL, *++p);
+}
+
+TEST_F(Array1dTest, given_entryPointer_when_preDecrement_then_ptrAdvancedBeforeEval)
+{
+  array1d arr(8, ZFP_RATE_PARAM_BITS);
+  arr(0) = VAL;
+
+  array1d::pointer p = &arr(1);
+
+  EXPECT_EQ(VAL, *--p);
+}
+
+TEST_F(Array1dTest, given_entryPointer_when_addToPointer_then_ptrAdvanced)
+{
+  array1d arr(8, ZFP_RATE_PARAM_BITS);
+  arr(2) = VAL;
+
+  array1d::pointer p = &arr(0);
+  p = p + 2;
+
+  EXPECT_EQ(VAL, *p);
+}
+
+TEST_F(Array1dTest, given_entryPointer_when_pointerPlusEquals_then_ptrAdvanced)
+{
+  array1d arr(8, ZFP_RATE_PARAM_BITS);
+  arr(2) = VAL;
+
+  array1d::pointer p = &arr(0);
+  p += 2;
+
+  EXPECT_EQ(VAL, *p);
+}
+
+TEST_F(Array1dTest, given_entryPointer_when_subtractFromPointer_then_ptrMovedBack)
+{
+  array1d arr(8, ZFP_RATE_PARAM_BITS);
+  arr(0) = VAL;
+
+  array1d::pointer p = &arr(2);
+  p = p - 2;
+
+  EXPECT_EQ(VAL, *p);
+}
+
+TEST_F(Array1dTest, given_entryPointer_when_pointerMinusEquals_then_ptrMovedBack)
+{
+  array1d arr(8, ZFP_RATE_PARAM_BITS);
+  arr(0) = VAL;
+
+  array1d::pointer p = &arr(2);
+  p -= 2;
+
+  EXPECT_EQ(VAL, *p);
+}
+
+TEST_F(Array1dTest, given_entryPointers_when_subtractPointers_then_resultIsEntryDifference)
+{
+  array1d arr(8, ZFP_RATE_PARAM_BITS);
+
+  int i2 = 2;
+  int i1 = 0;
+  array1d::pointer p2 = &arr(i2);
+  array1d::pointer p1 = &arr(i1);
+
+  EXPECT_EQ(i2 - i1, p2 - p1);
+}
+
+TEST_F(Array1dTest, given_sameEntryPointers_when_compareForEquality_then_resultTrue)
+{
+  array1d arr(8, ZFP_RATE_PARAM_BITS);
+
+  int i = 0;
+  array1d::pointer p1 = &arr(i) + 2;
+  array1d::pointer p2 = &arr(i + 2);
+
+  EXPECT_TRUE(p1 == p2);
+}
+
+TEST_F(Array1dTest, given_differentEntryPointers_when_compareForInequality_then_resultTrue)
+{
+  array1d arr(8, ZFP_RATE_PARAM_BITS);
+
+  int i = 0;
+  array1d::pointer p1 = &arr(i);
+  array1d::pointer p2 = &arr(i + 2);
+
+  EXPECT_TRUE(p1 != p2);
+}
+
 int main(int argc, char* argv[]) {
   ::testing::InitGoogleTest(&argc, argv);
   ::testing::Environment* const foo_env = ::testing::AddGlobalTestEnvironment(new Array1dTestEnv);
